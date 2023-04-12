4ad7effdbbb0b6b6d028e03642f611519bc66ed2
leejy12 <leejy31415@gmail.com>
2023-04-12 16:23:47 +0900

    add remaps

--- lua/leejy/remap.lua
+++ lua/leejy/remap.lua
@@ -3,3 +3,16 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>fs", vim.cmd.Ex)
 
 vim.keymap.set("n", "<C-a>", "ggVG")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
---
+++ lua/leejy/set.lua
@@ -0,0 +1,33 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.scrolloff = 4
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.showmatch = true
+
+vim.opt.syntax = "on"
+
+vim.opt.mouse = "a"
+
+vim.opt.ruler = true
+
+vim.g.lightline = {
+  active = {
+    left = {
+      { 'mode', 'paste' },
+      { 'gitbranch', 'readonly', 'filename', 'modified' },
+    },
+  },
+  component_function = {
+    gitbranch = 'gitbranch#name',
+  },
+}
+
+vim.cmd([[autocmd VimLeave * set guicursor=n-v-c:ver100-blinkwait530-blinkon530]])
