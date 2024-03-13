e177c6256eec628f7373c21d8d9beefc6f204144
leejy12 <leejy31415@gmail.com>
2024-03-13 16:00:24 +0900

    add filesystem, find_files keymap

--- lua/leejy/core/keymaps.lua
+++ lua/leejy/core/keymaps.lua
@@ -1,4 +1,8 @@
 -- set leader key to space
 vim.g.mapleader = " "
 
+-- Go to directory view (fs: filesystem)
+vim.keymap.set("n", "<leader>fs", vim.cmd.Ex)
 
+-- Telescope find_files (ff: find files)
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
