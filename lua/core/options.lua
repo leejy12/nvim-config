8a6f8bd3afdff2c3dd7240a35262b743538fabf2
Junyoung Lee <leejy31415@outlook.com>
2024-11-23 14:58:27 +0900

    comments

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,17 +1,14 @@
--- line numbers
-vim.opt.number         = true
-vim.opt.relativenumber = true
+vim.opt.number         = true -- Show line numbers
+vim.opt.relativenumber = true -- Set relative number lines
 
--- tabs & indentation
 vim.opt.tabstop    = 4    -- 4 spaces for tabs
 vim.opt.shiftwidth = 4    -- 4 spaces for indent width
 vim.opt.expandtab  = true -- expand tab to spaces
 vim.opt.autoindent = true -- copy indent from current line when starting new one
 
--- line wrapping
-vim.opt.wrap = false
+vim.opt.wrap = false -- Turn off line wrapping
 
-vim.opt.scrolloff = 4
+vim.opt.scrolloff = 4 -- Minimal number of screen lines to keep above and below the cursor
 
-vim.opt.ignorecase = true
+vim.opt.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search
 
