be9871d49c36a14b986ffd4c24cfa200a742e5bb
Junyoung Lee <leejy31415@outlook.com>
2024-11-30 15:02:46 +0900

    smartindent

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -9,6 +9,7 @@ vim.opt.tabstop    = 4    -- 4 spaces for tabs
 vim.opt.shiftwidth = 4    -- 4 spaces for indent width
 vim.opt.expandtab  = true -- expand tab to spaces
 vim.opt.autoindent = true -- copy indent from current line when starting new one
+vim.opt.smartindent = true
 
 vim.opt.wrap = false -- Turn off line wrapping
 
