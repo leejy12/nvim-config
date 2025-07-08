1bbdb45bdbed1edaa30c9f4339ee968840b20b5d
Junyoung Lee <leejy31415@outlook.com>
2025-07-08 22:26:25 +0900

    change yazi keymap to <leader>y

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -4,7 +4,7 @@ return {
   keys = {
     -- 👇 in this section, choose your own keymappings!
     {
-      "<leader>n",
+      "<leader>y",
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
